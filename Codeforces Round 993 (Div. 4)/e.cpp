dd25ccbeb5415577c7983e51dbe0f21f9de53985
oreinke <oreinke123@gmail.com>
2024-12-18 15:15:48 -0500

    Finished prob E for round 993 div4. Still cant get f working

--- Codeforces Round 993 (Div. 4)/e.cpp
+++ Codeforces Round 993 (Div. 4)/e.cpp
@@ -1,3 +1,13 @@
+// Major contest error was rushing to implement before looking to reduce further.
+// We failed to find subsituting x/k^n for y into l2 <= y <= r2,
+// and then merging this with the bound for x.
+// this would give a inequality for x where all valid x are valid members of the language
+// k^n can be atmost 32 diff numbers due to constraints, basically netting a O(1) sol.
+
+// BIG NOTE: Never use ceil. Floating point errors will occur.
+// instead we used: int lb = l2 / curK + ((l2 % curK == 0) ? 0 : 1);
+// same result, no fp bs
+
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -6,25 +16,26 @@ int main() {
     int t;
     cin >> t;
     for (int tc = 0; tc < t; tc++) {
-        int k, l1, r1, l2, r2;
+        unsigned long long k; 
+        int l1, r1, l2, r2;
         cin >> k >> l1 >> r1 >> l2 >> r2;
-
-        int kLim = r2 / l1;
 
         int origK = k;
         vector<int> ks;
         ks.push_back(1);
-        while (k <= kLim) {
-            ks.push_back(k);
+        while (k <= r2) {
+            ks.push_back((int) k);
             k *= origK;
         }
 
         long long count = 0;
-
         for (int curK : ks) {
-            int smallestMult = curK * l1;
-            start = smallest + min(l2 % curK, curK - (l2 % curK));
-
+            int lb = l2 / curK + ((l2 % curK == 0) ? 0 : 1);
+            lb = max(l1, lb);
+            int rb = min(r1, r2 / curK);
+            if (rb - lb >= 0) {
+                count += rb - lb + 1;
+            }
         }
 
         cout << count << endl;
--- Codeforces Round 993 (Div. 4)/f.cpp
+++ Codeforces Round 993 (Div. 4)/f.cpp
@@ -2,55 +2,78 @@
 
 using namespace std;
 
+// failed due to not further reducing the math required for this problem.
+// we can further reduce the sum calculation from n2 to 2n.
+// B = (b1a1 + b1a2) + (b2a1 + b2a2)
+// B = b1(a1 + a2) + b2(a1 + a2)
+// B = (a1 + a2) * (b1 + b2)
+// B = SumA * SumB
+
+// We also can apply this same approach to the updates.
+
 int main() {
     int n, m, q;
     cin >> n >> m >> q;
     vector<int> arrA(n);
-    vector<int> arrB(n);
+    vector<int> arrB(m);
+    long long sumA = 0;
+    long long sumB = 0;
     for (int i = 0; i < n; i++) {
         int num;
         scanf("%d", &num);
         arrA[i] = num;
+        sumA += num;
     }
 
     for (int i = 0; i < m; i++) {
         int num;
         scanf("%d", &num);
         arrB[i] = num;
+        sumB += num;
+    }
+
+    long long sum = sumA * sumB;
+    
+    
+    unordered_set<long long> possA;
+    unordered_set<long long> possB;
+    for (int i = 0; i < n; i++) {
+        possA.insert(sumA - arrA[i]);
+    }
+    
+    for (int i = 0; i < m; i++) {
+        possB.insert(sumB - arrB[i]);
+    }
+
+
+    for (int curQ = 0; curQ < q; curQ++) {
+        int query;
+        scanf("%d", &query);
+        query = sum - query;
+        bool found = false;
+        if (query == 0) {
+            if (possA.find(0) != possA.end() && possB.find(0) != possB.end() ) {
+                found = true;
+            }
+        }
+
+        int absQuery = abs(query); 
+        for (int i = 1; i * i <= absQuery; i++) {
+            if (absQuery % i == 0) {
+                if (query > 0 && ((possA.find(absQuery / i)) && possB.find(i))) {
+
+                }
+            }
+        }
+
+        if (found) {
+            printf("yes\n");
+        }
+        else {
+            printf("no\n");
+        }
     }
 
     
-    vector<long long> rowSums(n);
-    vector<long long> colSums(m, 0);
-    set<long long> possibleSums;
-    long long sum = 0;
-    for (int i = 0; i < n; i++) {
-        long long rSum = 0;
-        for (int j = 0; j < m; j++) {
-            rSum += arrA[i] * arrB[j];
-            sum += arrA[i] * arrB[j];
-            colSums[j] += arrA[i] * arrB[j];
-        }
-        rowSums[i] = rSum;
-    }
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            possibleSums.insert(rowSums[i] + colSums[j] - arrA[i] * arrB[j]);
-        }
-    }
-
-    for (int i = 0; i < q; i++) {
-        int query;
-        scanf("%d", &query);
-        if (possibleSums.find(sum - query) != possibleSums.end()) {
-            printf("YES\n");
-        }
-        else {
-            printf("NO\n");
-        }
-    }
-
-
-
+    
 }
